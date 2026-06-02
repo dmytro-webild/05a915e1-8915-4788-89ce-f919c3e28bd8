6a238154afac05229894ba699d5ac0f12b6f515c
bender <kudin.dima@gmail.com>
2026-06-02 06:47:28 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,13 @@ export const metadata: Metadata = {
 };
 
 
-const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
-  subsets: ["latin"],
-});
+
 const inter = Inter({
   variable: "--font-inter",
+  subsets: ["latin"],
+});
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -33,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
+        <body className={`${inter.variable} ${openSans.variable} antialiased`}>
           
           {children}
           <script
